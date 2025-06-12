7d776800266d6de3e2b558a24a5f97e386732315
Raneult0708 <vognitolaurent@gmail.com>
2025-06-12 08:37:32 +0100

    taff

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('registerForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Inscription soumise ! (Simulation)');
+    // Ici, vous intégrerez une requête AJAX vers Django plus tard.
+});
