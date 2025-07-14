4062d72a643028e41575522c37c3538a0dc10d88
Raneult0708 <vognitolaurent@gmail.com>
2025-07-15 00:44:22 +0100

    change

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('registerForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Inscription soumise ! (Simulation)');
-    // Ici, vous intégrerez une requête AJAX vers Django plus tard.
-});
--- todo_list/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('accueil', views.accueil, name='accueil'),
-    path('profil/', views.profil, name='profil'),
-    path('sign_up/', views.sign_up, name='sign_up'),
-    path('publish_ride/', views.publish_ride, name='publish_ride')
-]
